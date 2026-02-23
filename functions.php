0d90440aea2ad3c1f9578094eda4288c57159051
Ryan <ry_17ha_88@yahoo.com>
2026-02-23 10:48:58 +0800

    add some code

--- functions.php
+++ functions.php
@@ -5,8 +5,9 @@
  * @package YanskiWordPress
  */
 
-function yanski_enqueue_scripts() {
-    wp_enqueue_style( 'stylesheet', get_stylesheet_uri(), [], filemtime(get_template_directory() . './style.css'), 'all' );
-}
-add_action( 'wp_enqueue_scripts', 'yanski_enqueue_scripts');
+function yanski_enqueue_scripts() 
+    {
+        wp_enqueue_style( 'stylesheet', get_stylesheet_uri(), [], filemtime(get_template_directory() . './style.css'), 'all' );
+    }
+    add_action( 'wp_enqueue_scripts', 'yanski_enqueue_scripts');
 ?>

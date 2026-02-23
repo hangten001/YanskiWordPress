e2781f214ea6283bc266029acc602f870c6c6410
Ryan <ry_17ha_88@yahoo.com>
2026-02-23 11:08:51 +0800

    add code

--- functions.php
+++ functions.php
@@ -5,8 +5,7 @@
  * @package YanskiWordPress
  */
 
-function yanski_enqueue_scripts() 
-{
+function yanski_enqueue_scripts(){
     wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], 
     filemtime(get_template_directory() . './style.css'), 'all');
 }
